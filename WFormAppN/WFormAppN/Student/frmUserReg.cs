798b7cbe32c00d3114e201d77b9cdf7f65d36b37
nupurbhatt1998 <nupurbhatt2014@gmail.com>
2020-02-07 16:21:43 +0530

    add new form for student registration

---
+++ WFormAppN/WFormAppN/Student/frmUserReg.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFormAppN.Student
+{
+    public partial class frmUserReg : Form
+    {
+        public frmUserReg()
+        {
+            InitializeComponent();
+        }
+    }
+}
